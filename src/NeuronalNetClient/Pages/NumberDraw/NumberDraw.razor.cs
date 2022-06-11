6b4827d572dafa4d3e4a3b23b4c28c153e871415
Jan <MapManagement@protonmail.com>
2022-06-11 22:40:14 +0200

    Add NumberDraw page, edit index page

---
+++ src/NeuronalNetClient/Pages/NumberDraw/NumberDraw.razor.cs
@@ -0,0 +1,23 @@
+namespace NeuronalNetClient.Pages.NumberDraw
+{
+    public partial class NumberDraw
+    {
+        #region Overrides
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void OnNumberButtonClicked(int number)
+        {
+            return;
+        }
+
+        #endregion
+    }
+}
