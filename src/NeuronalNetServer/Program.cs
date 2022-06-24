e96203a51a2ef631f4f8778a04af0a446971f53f
Jan <MapManagement@protonmail.com>
2022-06-24 22:50:25 +0200

    Add multithreading

--- src/NeuronalNetServer/Program.cs
+++ src/NeuronalNetServer/Program.cs
@@ -1,17 +1,8 @@
-using NeuronalNetServer.Services;
+using NeuronalNetServer.Helpers;
 
-var builder = WebApplication.CreateBuilder(args);
+Thread grpcThread = new Thread(ServiceInitialization.StartGrpcService);
+// Thread neuralNetThread = new Thread();
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+grpcThread.Start();
+// neuralNetThread.Start();
 
-// Add services to the container.
-builder.Services.AddGrpc();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-app.MapGrpcService<UploadService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
-app.Run();
